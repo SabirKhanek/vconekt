6f4da63534fb851388a6be65199311acd0891ff7
DESKTOP-CF9SRM6\lenovo <danifazil99@gmail.com>
2024-09-24 12:37:03 +0500

    New directs added minue www

--- next.config.js
+++ next.config.js
@@ -17,51 +17,40 @@ const nextConfig = {
         has: [
           {
             type: 'host',
-            value: 'vconekt.com'
+            value: 'www.vconekt.com'
           }
         ],
-        destination: 'https://www.vconekt.com/:path*',
+        destination: 'https://vconekt.com/:path*',
         permanent: true
       },
       {
         source: '/projects/:subpath*',
-        has: [
-          {
-            type: 'host',
-            value: 'vconekt.com'
-          }
-        ],
-        destination: 'https://www.vconekt.com/projects/:subpath*',
+        destination: 'https://vconekt.com/projects/:subpath*',
         permanent: true
       },
       {
         source: '/services/artificial intelligence',
-        destination: 'https://www.vconekt.com/services/artificial-intelligence',
+        destination: 'https://vconekt.com/services/artificial-intelligence',
         permanent: true
       },
       {
         source: '/services/mobile_app_dev',
-        destination: 'https://www.vconekt.com/services/mobile-app-dev',
+        destination: 'https://vconekt.com/services/mobile-app-dev',
         permanent: true
       },
       {
         source: '/services/ui_ux',
-        destination: 'https://www.vconekt.com/services/ui-ux-graphic-design',
+        destination: 'https://vconekt.com/services/ui-ux-graphic-design',
         permanent: true
       },
       {
         source: '/services/web_design_and_development',
-        destination: 'https://www.vconekt.com/services/website-development',
+        destination: 'https://vconekt.com/services/website-development',
         permanent: true
       },
       {
         source: '/www.website.com',
-        destination: 'https://www.vconekt.com',
-        permanent: true
-      },
-      {
-        source: '/:path*',
-        destination: 'https://www.vconekt.com',
+        destination: 'https://vconekt.com',
         permanent: true
       }
     ];
